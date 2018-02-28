b359007a30b08893ba6980c7d4f0552ec26fa953
Eduardo Germano da Silva <eduardo.germano@lojasrenner.com.br>
2018-02-28 16:17:48 -0300

    including get_antonimos

--- diciopybr.py
+++ diciopybr.py
@@ -6,6 +6,7 @@ class Termos:
 
     def __init__(self):
         self.url_sinonimos = "https://sinonimos.com.br/"
+        self.url_antonimos = "https://antonimos.com.br/"
 
     def get_sinonimos(self, termo):
         url       = self.url_sinonimos + "{}/".format(termo)
@@ -15,9 +16,9 @@ class Termos:
         sinonimos = []
 
         for div in soup.find_all('a'):
-            div   = str(div).replace("</a>", "")
-            start = div.find(">") + 1
-            sinonimos.append(div[start:])
+            d     = str(div).replace("</a>", "")
+            start = d.find(">") + 1
+            sinonimos.append(d[start:])
 
         for div in soup.find_all('span'):
             sinonimos.append(str(div).replace("<span>", "").replace("</span>", ""))
@@ -25,5 +26,19 @@ class Termos:
         return sinonimos
 
     def get_antonimos(self, termo):
+        url       = self.url_antonimos + "{}/".format(termo)
+        html      = requests.get(url).text
+        soup      = BeautifulSoup(html, 'html.parser')
+        soup      = BeautifulSoup(str(soup.select('div.s-wrapper')), 'html.parser')
+        antonimos = []
+
+        for div in soup.find_all('a'):
+            d = str(div).replace("</a>", "")
+            start = d.find(">") + 1
+            antonimos.append(d[start:])
+
+        return antonimos
+
+    def get_sentidos(self, termo):
 
         pass
