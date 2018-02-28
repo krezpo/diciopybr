6790c1649bce59bd0f84d561c3d3607bb582ef3b
Eduardo Germano da Silva <eduardo.germano@lojasrenner.com.br>
2018-02-28 17:37:33 -0300

    including get_sentidos

--- diciopybr.py
+++ diciopybr.py
@@ -3,12 +3,21 @@ from bs4 import BeautifulSoup
 from bs4.element import Tag
 
 class Termos:
+    '''
+    Coded by Eduardo Germano
+    '''
 
     def __init__(self):
         self.url_sinonimos = "https://sinonimos.com.br/"
         self.url_antonimos = "https://antonimos.com.br/"
 
     def get_sinonimos(self, termo):
+        '''
+        Retorna a lista de sinonimos de um determinado termo.
+
+        :param termo:
+        :return list:sinonimos
+        '''
         url       = self.url_sinonimos + "{}/".format(termo)
         html      = requests.get(url).text
         soup      = BeautifulSoup(html, 'html.parser')
@@ -26,6 +35,13 @@ class Termos:
         return sinonimos
 
     def get_antonimos(self, termo):
+        '''
+        Retorna a lista de antonimos de um dado termo.
+
+        :param termo:
+        :return list:antonimos
+        '''
+
         url       = self.url_antonimos + "{}/".format(termo)
         html      = requests.get(url).text
         soup      = BeautifulSoup(html, 'html.parser')
@@ -33,12 +49,28 @@ class Termos:
         antonimos = []
 
         for div in soup.find_all('a'):
-            d = str(div).replace("</a>", "")
+            d     = str(div).replace("</a>", "")
             start = d.find(">") + 1
             antonimos.append(d[start:])
 
         return antonimos
 
     def get_sentidos(self, termo):
+        '''
+        Retorna a lista de sentidos de um determinado termo.
 
-        pass
+        :param termo:
+        :return list:sentidos
+        '''
+
+        url      = self.url_sinonimos + "{}/".format(termo)
+        html     = requests.get(url).text
+        soup     = BeautifulSoup(html, 'html.parser')
+        soup     = BeautifulSoup(str(soup.select('div.sentido')), 'html.parser')
+        sentidos = []
+
+        for s in soup:
+            if isinstance(s, Tag):
+                sentidos.append(str(s).replace('<div class="sentido">', "").replace(":</div>", ""))
+
+        return sentidos
